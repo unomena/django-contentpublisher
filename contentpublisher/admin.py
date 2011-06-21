b1e7714badbc7233d4db275ef8fb6fb40705e51f
Euan Jonker <euan@unomena.com>
2011-06-21 08:54:09 +0200

    Initial commit.

---
+++ contentpublisher/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from contentpublisher.models import ContentUpgrade
+
+admin.site.register(ContentUpgrade)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-contentpublisher',
+    version='0.0.1',
+    description='Django qa publishing app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Unomena',
+    author_email='dev@unomena.com',
+    license='BSD',
+    packages = find_packages(),
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
