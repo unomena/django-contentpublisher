11fd394ed9b5bc83549fcef36f00b7a38f76efe9
Euan Jonker <euan@unomena.com>
2011-12-15 12:53:49 +0200

    Added view, url & template for content_list.

---
+++ contentpublisher/urls.py
@@ -0,0 +1,24 @@
+'''
+Created on 15 Dec 2011
+
+@author: euan
+'''
+from django.conf.urls.defaults import *
+from django.contrib.auth.decorators import login_required
+
+from django.views import generic as generic_views
+
+from django.contrib import admin
+admin.autodiscover()
+
+from contentpublisher import models
+
+urlpatterns = patterns('contentpublisher.views',
+                       
+    url(r'^contentpublisher/$',
+        login_required(generic_views.ListView.as_view(queryset=models.ContentUpgrade.objects.all(),
+                                                      template_name='contentpublisher/content_list.html')
+                       ),
+        name='content_publisher_content_list'),
+        
+)
