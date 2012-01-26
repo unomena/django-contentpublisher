96d014e2664a1a5bcdc444253260106865598410
Euan Jonker <euan@unomena.com>
2012-01-26 11:05:31 +0200

    Added M2M object.

--- contentpublisher/models.py
+++ contentpublisher/models.py
@@ -27,3 +27,24 @@ class ContentUpgrade(models.Model):
     #--------------------------------------------------------------------------
     def __unicode__(self):
         return str(self.object)
+
+#==============================================================================
+class ContentM2MUpgrade(models.Model):
+    """
+    Stores sql to upgrade objects on another system.
+    """
+    object_content_type = models.ForeignKey(ContentType, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    object = generic.GenericForeignKey(ct_field='object_content_type', fk_field='object_id')
+    sql = models.TextField()
+    date_time = models.DateTimeField(auto_now=True)
+    must_publish = models.BooleanField(default=True)
+    
+    #==========================================================================
+    class Meta:
+        ordering = ('-date_time',)
+#        unique_together = (('object_content_type','object_id'),)
+    
+    #--------------------------------------------------------------------------
+    def __unicode__(self):
+        return str(self.object)
--- contentpublisher/tasks.py
+++ contentpublisher/tasks.py
@@ -7,7 +7,7 @@ from django import db
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
-from contentpublisher.models import ContentUpgrade
+from contentpublisher.models import ContentUpgrade, ContentM2MUpgrade
 
 #------------------------------------------------------------------------------
 def check_sql(sql):
@@ -51,7 +51,26 @@ def track_m2m_changed_object(sender, instance, **kwargs):
     """
     Track a saved object
     """
-    track_object(instance)
+    sqls = []
+    if settings.CONTENT_STAGING:
+        for q in db.connection.queries:
+            if check_sql(q['sql']) and q['sql'] not in sqls:
+                sqls.append(q['sql'])
+                
+        db.reset_queries()
+        
+    if sqls:
+        try:
+            cu = ContentM2MUpgrade.objects.get(object_content_type=ContentType.objects.get_for_model(instance.__class__),
+                                      object_id=instance.id)
+            cu.sql = '%s;' % ';\n'.join(sqls)
+            cu.must_publish = True
+            cu.save()
+            
+        except ContentM2MUpgrade.DoesNotExist:
+            ContentM2MUpgrade.objects.create(object_content_type=ContentType.objects.get_for_model(instance.__class__),
+                                          object_id=instance.id, 
+                                          sql='%s;' % ';\n'.join(sqls))
 
 #------------------------------------------------------------------------------
 def track_deleted_object(sender, instance, **kwargs):
